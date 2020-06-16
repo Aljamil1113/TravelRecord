a078769e55a53fc2739d835b237d8fa779bf0953
Aljamil1113 <aarazasaljamil@gmail.com>
2020-06-16 14:29:08 -0700

    Profile page show categories using Linq

--- TravelRecord/TravelRecord/ProfilePage.xaml.cs
+++ TravelRecord/TravelRecord/ProfilePage.xaml.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
-
+using TravelRecord.Model;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,5 +18,38 @@ namespace TravelRecord
 		{
 			InitializeComponent ();
 		}
-	}
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+			using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+				var postTable = conn.Table<Post>().ToList();
+
+				//var categories = (from p in postTable
+				//				  orderby p.CategoryId
+				//				  select p.CategoryName).Distinct().ToList();
+
+				var categories = postTable.OrderBy(p => p.CategoryId).Select(p => p.CategoryName).Distinct().ToList();
+
+				Dictionary<string, int> categoriesCount = new Dictionary<string, int>();
+
+                foreach (var category in categories)
+                {
+					//var countCategory = (from post in postTable
+					//					 where post.CategoryName == category
+					//					 select post).ToList().Count;
+
+					var countCategory = postTable.Where(p => p.CategoryName == category).ToList().Count;
+
+					categoriesCount.Add(category, countCategory);
+                }
+
+				categoriesListView.ItemsSource = categoriesCount;
+
+				postCountLabel.Text = postTable.Count.ToString();
+            }
+        }
+    }
 }
