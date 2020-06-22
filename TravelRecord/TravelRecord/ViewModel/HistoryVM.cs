a10d2fa7c573d1ed286ad914e2d5c8fab90e788f
Aljamil1113 <aarazasaljamil@gmail.com>
2020-06-21 19:15:25 -0700

    ObservableCollection in HistoryPage

---
+++ TravelRecord/TravelRecord/ViewModel/HistoryVM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using TravelRecord.Model;
+
+namespace TravelRecord.ViewModel
+{
+    public class HistoryVM
+    {
+        public ObservableCollection<Post> Posts { get; set; }
+
+        public HistoryVM()
+        {
+            Posts = new ObservableCollection<Post>();
+        }
+
+        public void UpdatePosts()
+        {
+            Posts.Clear();
+            var posts = Post.Read();
+            foreach (var post in posts)
+            {
+                Posts.Add(post);
+            }
+        }
+    }
+}
